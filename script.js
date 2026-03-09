2ddbefedc5d2c24d40ac9b84c6303ade572b32af
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-09 09:45:32 +0100

    create function with array, bad result :(

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Prompt Utente Momentaneo
-let infoUtente = 313;
+let infoUtente = 312;
 
 //Array da verificare con la funzione reverse()
 const toCheck = [];
@@ -10,6 +10,19 @@ console.log(toCheck);
 //Funzione Palindromo
 function toPalindrome (input) {
 
-    
+    if (input == input.reverse()) {
 
+        const positiveOut = 'La parola è palindroma';
+        console.log(positiveOut);
+
+    } else {
+
+        const negativeOut = 'La parola non è palindroma';
+        console.log(negativeOut);
+
+    }
 }
+
+toPalindrome(toCheck)
+console.log(toCheck); // Ho scoperto che la funzione reverse non è adatto per verificare 
+                      // se la parola è palindroma dato che inverte tutto l'array e non il singolo valore :(
