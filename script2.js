c17fa11958574a644dd40083f1756ca5d4ef90d4
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-09 23:03:37 +0100

    complete function

--- script2.js
+++ script2.js
@@ -10,6 +10,8 @@ let paOdi = prompt('Pari o dispari?');
 let paOdiUpper = paOdi.toUpperCase();
 console.log(paOdiUpper);
 let numUtente = prompt('Scegli un numero da 1 a 5');
+let numUtente2 = Number(numUtente);
+console.log(numUtente2);
 
 //Se l'utente sbaglia a inserire i dati
 if (paOdiUpper !== 'PARI' && paOdiUpper !== 'DISPARI') {
@@ -23,9 +25,32 @@ if (paOdiUpper !== 'PARI' && paOdiUpper !== 'DISPARI') {
 }
 
 //Computer Random
-function computerRandom (input) {
+function computerRandom (inputNum, inputPD) {
 
     let numSorted = Math.floor((Math.random() * 5) + 1);
-    console.log(numSorted);
+    let numSorted2 = Number(numSorted);
+    console.log(numSorted2);
 
+    let somma = inputNum + numSorted2;
+    console.log(somma);
+    
+
+    if (somma % 2 == 0 && inputPD == 'PARI') {
+
+        let victory = alert('Complimenti, hai vinto')
+        return victory;
+
+    } else if (somma % 2 != 0 && inputPD == 'DISPARI') {
+
+        let victory = alert('Complimenti, hai vinto')
+        return victory;
+
+    } else {
+
+        let defeat = alert('Oh no! Hai perso..');
+        return defeat;
+
+    }
 }
+
+computerRandom (numUtente2, paOdiUpper);
