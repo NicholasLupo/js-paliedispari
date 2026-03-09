9a4896f9098165e3fb31c02e2c423c89802c54f7
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-09 22:43:46 +0100

    add error value system

--- script2.js
+++ script2.js
@@ -7,4 +7,17 @@
 
 //L'utente inserisce le sue scelte
 let paOdi = prompt('Pari o dispari?');
+let paOdiUpper = paOdi.toUpperCase();
+console.log(paOdiUpper);
 let numUtente = prompt('Scegli un numero da 1 a 5');
+
+//Se l'utente sbaglia a inserire i dati
+if (paOdiUpper !== 'PARI' && paOdiUpper !== 'DISPARI') {
+
+    let error = alert('Valore inserito errato, riavvia la pagina e riprova');
+
+} else if (numUtente > 5 || numUtente <= 0) {
+
+    let error = alert('Valore inserito errato, riavvia la pagina e riprova');
+
+}
