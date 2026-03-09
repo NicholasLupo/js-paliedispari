240ca1e11c5a1b070f67e03d442c6f1acef5acc1
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-09 09:32:50 +0100

    add array

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-//Prompt Utente
-let infoUtente;
+//Prompt Utente Momentaneo
+let infoUtente = 313;
+
+//Array da verificare con la funzione reverse()
+const toCheck = [];
+toCheck.unshift(infoUtente);
+console.log(toCheck);
+
 
 //Funzione Palindromo
 function toPalindrome (input) {
+
     
+
 }
