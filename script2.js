77093eaa4bb8c91b75850f6f2536a8d39f2ec2be
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-09 23:11:50 +0100

    fix function with error input and if

--- script2.js
+++ script2.js
@@ -1,10 +1,3 @@
-// **Pari e Dispari**
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-
 //L'utente inserisce le sue scelte
 let paOdi = prompt('Pari o dispari?');
 let paOdiUpper = paOdi.toUpperCase();
@@ -13,44 +6,47 @@ let numUtente = prompt('Scegli un numero da 1 a 5');
 let numUtente2 = Number(numUtente);
 console.log(numUtente2);
 
-//Se l'utente sbaglia a inserire i dati
-if (paOdiUpper !== 'PARI' && paOdiUpper !== 'DISPARI') {
-
-    let error = alert('Valore inserito errato, riavvia la pagina e riprova');
-
-} else if (numUtente > 5 || numUtente <= 0) {
-
-    let error = alert('Valore inserito errato, riavvia la pagina e riprova');
-
-}
-
 //Computer Random
-function computerRandom (inputNum, inputPD) {
+function computerRandom(inputNum, inputPD) {
 
-    let numSorted = Math.floor((Math.random() * 5) + 1);
-    let numSorted2 = Number(numSorted);
-    console.log(numSorted2);
+    //Se l'utente sbaglia a inserire i dati
+    if (inputPD !== 'PARI' && inputPD !== 'DISPARI') {
 
-    let somma = inputNum + numSorted2;
-    console.log(somma);
-    
+        let error = alert('Valore inserito errato, riavvia la pagina e riprova');
+        return error;
 
-    if (somma % 2 == 0 && inputPD == 'PARI') {
+    } else if (inputNum > 5 || inputNum <= 0) {
 
-        let victory = alert('Complimenti, hai vinto')
-        return victory;
-
-    } else if (somma % 2 != 0 && inputPD == 'DISPARI') {
-
-        let victory = alert('Complimenti, hai vinto')
-        return victory;
+        let error = alert('Valore inserito errato, riavvia la pagina e riprova');
+        return error;
 
     } else {
 
-        let defeat = alert('Oh no! Hai perso..');
-        return defeat;
+        //Pescata Random e somma dei valori
+        let numSorted = Math.floor((Math.random() * 5) + 1);
+        let numSorted2 = Number(numSorted);
+        console.log(numSorted2);
+        let somma = inputNum + numSorted2;
+        console.log(somma);
 
+        //Verifica se l'utente ha vinto
+        if (somma % 2 == 0 && inputPD == 'PARI') {
+
+            let victory = alert('Complimenti, hai vinto')
+            return victory;
+
+        } else if (somma % 2 != 0 && inputPD == 'DISPARI') {
+
+            let victory = alert('Complimenti, hai vinto')
+            return victory;
+
+        } else {
+
+            let defeat = alert('Oh no! Hai perso..');
+            return defeat;
+
+        }
     }
 }
 
-computerRandom (numUtente2, paOdiUpper);
+computerRandom(numUtente2, paOdiUpper);
